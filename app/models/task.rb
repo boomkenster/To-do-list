c7d9f4ed59248efaaad1216ff2febe436e573a6b
Kenny <chenkenn@gmail.com>
2015-08-12 09:15:59 -0600

    add default image

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,7 @@ class Task < ActiveRecord::Base
   validates :title, presence:true
   validate :start_date_cannot_be_in_the_past
   validate :due_date_cannot_be_in_the_past           
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://www.gianteagle.com/PageFiles/11386/thumbnail_Petcare_SmallAnimals_05012012.png"
 validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   def default_values
