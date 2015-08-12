736de5a2de96245db27ef18d841595740842aea7
Kenny <chenkenn@gmail.com>
2015-08-11 23:33:05 -0600

    filtering complete

--- app/assets/javascripts/task-list.js
+++ app/assets/javascripts/task-list.js
@@ -8,5 +8,26 @@ $(document).ready(function(){
   $('.change-status').on('click', function(data){
     data.preventDefault();
   })
+
+  function findMatch(element, searchTerm){
+    var output = false;
+    $(element).children().each(function(i,e){
+      if ($(e).html().match(searchTerm)){
+        output = true;
+      }
+    });
+    return output;
+  }
+
+  $('.search-box').on('keyup', function(){
+    var searchTerm = new RegExp($(".search-box").val(), "i");
+    $('.all-task').children().each(function(index, element){
+      if (findMatch(element, searchTerm)) {
+        $(element).show();
+      } else {
+        $(element).hide();
+      }
+    });
+  });
 });
 
